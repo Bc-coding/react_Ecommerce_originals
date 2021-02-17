2c5f78cf8d49fe1d7d16652d769cc19d1e707a5e
Belle Carrie <bc.coding@hotmail.com>
2021-02-17 13:35:56 +0000

    added the UserContext to Sidebar and the condition to render the checkout link

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,9 +6,11 @@ import { FaTimes } from "react-icons/fa";
 import { links } from "../utils/constants";
 
 import { useProductsContext } from "../context/products_context";
+import { useUserContext } from "../context/user_context";
 
 function Sidebar() {
   const { isSidebarOpen, closeSidebar } = useProductsContext();
+  const { myUser } = useUserContext();
 
   return (
     <SidebarContainer>
@@ -32,11 +34,13 @@ function Sidebar() {
               </li>
             );
           })}
-          <li>
-            <Link to="/checkout" onClick={closeSidebar}>
-              checkout
-            </Link>
-          </li>
+          {myUser && (
+            <li>
+              <Link to="/checkout" onClick={closeSidebar}>
+                checkout
+              </Link>
+            </li>
+          )}
         </ul>
         <CartButtons />
       </aside>
