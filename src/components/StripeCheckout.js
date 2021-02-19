58c8f0cde195f539af25959999beee9287267f23
Belle Carrie <bc.coding@hotmail.com>
2021-02-19 14:52:06 +0000

    installed Stripe and Axios dependencies and included imports from Stripe and userCartContext/useCartContext to Stripecheckout.js

--- src/components/StripeCheckout.js
+++ src/components/StripeCheckout.js
@@ -2,6 +2,23 @@ import React from "react";
 import styled from "styled-components";
 import Checkout from "../pages/Checkout";
 
+// adding imports from Stripe
+import { loadStripe } from "@stripe/stripe-js";
+import {
+  CardElement,
+  useStripe,
+  Elements,
+  useElements,
+} from "@stripe/react-stripe-js";
+
+import axios from "axios";
+import { useCartContext } from "../context/cart_context";
+import { useUserContext } from "../context/user_context";
+import { formatPrice } from "../utils/helpers";
+import { useHistory } from "react-router-dom";
+
+const promise = loadStripe(process.env.REACT_APP_STRIPE_PUBLIC_KEY);
+
 const CheckoutForm = () => {
   return <h2>Hello from Stripe checkout</h2>;
 };
@@ -9,7 +26,9 @@ const CheckoutForm = () => {
 const StripeCheckout = () => {
   return (
     <Wrapper>
-      <CheckoutForm />
+      <Elements stripe={promise}>
+        <CheckoutForm />
+      </Elements>
     </Wrapper>
   );
 };
